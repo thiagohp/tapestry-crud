5ae447c4713c81b166c20be04c6c056467ca9868
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2008-12-14 05:20:21 +0000

    Improved ActionLinks component, adding a view link and using images instead of text.

--- src/main/java/br/com/arsmachina/tapestrycrud/components/ActionLinks.java
+++ src/main/java/br/com/arsmachina/tapestrycrud/components/ActionLinks.java
@@ -14,30 +14,50 @@
 
 package br.com.arsmachina.tapestrycrud.components;
 
+import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.IncludeStylesheet;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Grid;
 
+import br.com.arsmachina.tapestrycrud.Constants;
 
 /**
- * Component that renders the action links in a listing page.
- * It is meant to be used in a {@link Grid} column.
- * <a href="http://ars-machina.svn.sourceforge.net/viewvc/ars-machina/example/trunk/src/main/webapp/project/ListProject.tml?view=markup"
- * 		>Ars Machina Project Example</a>.
+ * Component that renders the action links in a listing page. It is meant to be used in a
+ * {@link Grid} column. <a
+ * href="http://ars-machina.svn.sourceforge.net/viewvc/ars-machina/example/trunk/src/main/webapp/project/ListProject.tml?view=markup"
+ * >Ars Machina Project Example</a>. The default icons used are taken from the <a
+ * href="http://www.famfamfam.com/lab/icons/silk/">Silk</a> icon set (Creative Commons Attribution
+ * 2.5 License).
  * 
  * @author Thiago H. de Paula Figueiredo
  */
+@IncludeStylesheet(Constants.TAPESTRY_CRUD_CSS_ASSET)
 public class ActionLinks {
-	
+
+	private static final String IMAGES_ASSET_ROOT = "asset:classpath:/br/com/arsmachina/tapestrycrud/components/images/";
+
+	private static final String DEFAULT_EDIT_ICON_ASSET = IMAGES_ASSET_ROOT + "edit.png";
+
+	private static final String DEFAULT_DELETE_ICON_ASSET = IMAGES_ASSET_ROOT + "delete.png";
+
+	private static final String DEFAULT_VIEW_ICON_ASSET = IMAGES_ASSET_ROOT + "view.png";
+
 	/**
-	 * Name of the page that is used to edits objects listed in  this page.
+	 * Name of the page that is used to edits objects listed in this page.
 	 */
-	@Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
-	@Property
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
 	@SuppressWarnings("unused")
 	private String editPage;
-	
+
+	/**
+	 * Name of the page that is used to edits objects listed in this page.
+	 */
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
+	@SuppressWarnings("unused")
+	private String viewPage;
+
 	/**
 	 * Show the edit link?
 	 */
@@ -53,7 +73,15 @@ public class ActionLinks {
 	@Property
 	@SuppressWarnings("unused")
 	private boolean remove;
-	
+
+	/**
+	 * Show the view link?
+	 */
+	@Parameter(value = "false")
+	@Property
+	@SuppressWarnings("unused")
+	private boolean view;
+
 	/**
 	 * The object that the links will refer to.
 	 */
@@ -61,5 +89,55 @@ public class ActionLinks {
 	@Property
 	@SuppressWarnings("unused")
 	private Object object;
-	
+
+	@Parameter(defaultPrefix = BindingConstants.ASSET, value = DEFAULT_EDIT_ICON_ASSET)
+	@Property
+	@SuppressWarnings("unused")
+	private Asset editIcon;
+
+	@Parameter(defaultPrefix = BindingConstants.ASSET, value = DEFAULT_VIEW_ICON_ASSET)
+	@Property
+	@SuppressWarnings("unused")
+	private Asset viewIcon;
+
+	@Parameter(defaultPrefix = BindingConstants.ASSET, value = DEFAULT_DELETE_ICON_ASSET)
+	@Property
+	@SuppressWarnings("unused")
+	private Asset deleteIcon;
+
+	/**
+	 * Returns the value of the <code>editPage</code> property.
+	 * 
+	 * @return a {@link String}.
+	 */
+	public String getEditPage() {
+
+		if (edit && (editPage == null || editPage.trim().length() == 0)) {
+			
+			throw new IllegalArgumentException(
+					"When parameter edit is true, parameter editPage must have a non-empty value");
+			
+		}
+
+		return editPage;
+	}
+
+	/**
+	 * Returns the value of the <code>viewPage</code> property.
+	 * 
+	 * @return a {@link String}.
+	 */
+	public String getViewPage() {
+
+		if (view && viewPage == null || viewPage.trim().length() == 0) {
+			
+			throw new IllegalArgumentException(
+					"When parameter view is true, parameter viewPage must have a non-empty value");
+			
+		}
+		
+		return viewPage;
+		
+	}
+
 }
